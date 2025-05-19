d52e433fd6b2481cf6377dce4cf964f22c165ceb
Senthilnathan Mani <31062212+SENTHILnew@users.noreply.github.com>
2025-05-19 16:14:57 +0530

    adding changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ViewChild, viewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { LOAD_WASM, NgxScannerQrcodeComponent, ScannerQRCodeResult } from 'ngx-scanner-qrcode';
+import { WebcamModule } from 'ngx-webcam';
+
+LOAD_WASM('assets/wasm/ngx-scanner-qrcode.wasm').subscribe();
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, NgxScannerQrcodeComponent, WebcamModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  @ViewChild('action') scannerComponent!: NgxScannerQrcodeComponent;
+
   title = 'camera-poc';
+
+  showBarcodeScan = false;
+
+  resultText = '';
+
+  constructor(private readonly ref: ChangeDetectorRef) {
+
+  }
+
+  openQrCodeScanner() {
+    this.showBarcodeScan = true;
+    this.ref.markForCheck();
+    this.scannerComponent?.start();
+  }
+
+  closBarCodeScan() {
+    this.showBarcodeScan = false;
+  }
+
+  onEvent(event: ScannerQRCodeResult[]) {
+    this.resultText = event[0].value
+    this.closBarCodeScan();
+  }
 }
